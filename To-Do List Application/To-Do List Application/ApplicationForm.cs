bcdb0331c6a73fdb8f2614ff887df5f347d8f3fd
HighCritSoldier <rgaeta947@gmail.com>
2023-10-05 17:17:00 -0400

    Initial Commit

---
+++ To-Do List Application/To-Do List Application/ApplicationForm.cs
@@ -0,0 +1,10 @@
+namespace To_Do_List_Application
+{
+    public partial class ApplicationForm : Form
+    {
+        public ApplicationForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
